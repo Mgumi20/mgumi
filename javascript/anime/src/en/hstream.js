e363b84bedfc3c26a126371389f42d59daf36df2
Mgumi20 <bayrout2025x2@outlook.com>
2025-07-15 21:39:01 +0000

    Update hstream.js

--- javascript/anime/src/en/hstream.js
+++ javascript/anime/src/en/hstream.js
@@ -11,7 +11,7 @@ const mangayomiSources = [{
     "hasCloudflare": true,
     "sourceCodeUrl": "",
     "apiUrl": "",
-    "version": "1.1.1", // تم تحديث الإصدار
+    "version": "1.0.12",
     "isManga": false,
     "itemType": 1,
     "isFullData": false,
