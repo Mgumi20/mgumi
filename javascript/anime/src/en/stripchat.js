2969fc1f3c4c0386303e2454f11b9314e1146d9e
Mgumi20 <bayrout2025x2@outlook.com>
2025-07-17 18:18:17 +0000

    Update stripchat.js

--- javascript/anime/src/en/stripchat.js
+++ javascript/anime/src/en/stripchat.js
@@ -11,7 +11,7 @@ const mangayomiSources = [{
     "hasCloudflare": true,
     "sourceCodeUrl": "",
     "apiUrl": "",
-    "version": "1.0.4",
+    "version": "1.0.5",
     "isManga": false,
     "itemType": 1,
     "isFullData": false,
